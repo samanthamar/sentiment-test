00fd2abe657c862ddb7c562d2bf62c2451842d30
samanthamar <samantha.mar017@gmail.com>
2019-12-02 14:01:21 -0500

    add journal entries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Journal from './Journal';
 
 function App() {
+
+  const [journal, setJournal] = useState([]); 
+  const [data, setData] = useState({"log": "", "sentiment": ""});
+
+  const handleChange = (event) => {
+    // Only get the data once user is done typing
+    // Need to hook this up to the backend to get sentiment polarity
+    const userData = {"log": event.target.value, "sentiment": 0.0};
+    setData(userData);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Create a copy of existing journal entries
+    const updateJournal = [...journal];
+    // Add new entry
+    updateJournal.push(data)
+    // Replace old journal with new journal
+    setJournal(updateJournal);
+    // Reset the text area
+    setData({"log": "", "sentiment": ""})
+    console.log(journal)
+  }
+
+  const clearText = () => {
+    setData({"log": "", "sentiment": ""})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Sentiment Journal</h1>
+      <form onSubmit={handleSubmit}>
+          <div 
+            className="form-group"
+            style={{marginLeft: '50px',
+            marginRight: '50px'}}>
+              <textarea 
+                className="form-control" rows="3"
+                value={data.log}
+                onChange={handleChange}
+                placeholder="Input journal entry"/> 
+          </div>
+
+          <div className="form-group">
+            <input 
+              className="btn btn-primary mb-2" 
+              type="submit"
+              style={{marginRight: '20px'}}/>
+            <input 
+              className="btn btn-danger mb-2" 
+              type="reset"
+              onClick={clearText}/>
+          </div>
+
+      </form>
+
+      {/* Display the journal entries */}
+      <Journal log={journal}/>
+
+  </div>
   );
 }
 
---
+++ src/Journal.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Journal = (props) => {
+
+    const renderRow = props.log.map((data) =>
+        <tr key={Date.now}>
+            <td>{new Date().toISOString().slice(0,10).split('T')[0]}</td>
+            <td>{data.log}</td>
+            <td>{data.sentiment}</td>
+        </tr>
+    );
+
+    // Output in card style instead
+    // const cards = props.log.map((data) =>
+    //     <div className="card" style={{width: '18rem', margin: '10px'}}>
+    //         <div className="card-body">
+    //             <h5 className="card-title">Date</h5>
+    //             <h6 className="card-subtitle mb-2 text-muted">Journal</h6>
+    //             <p className="card-text">{data.log}</p>
+    //             <h6 className="card-subtitle mb-2 text-muted">Sentiment</h6>
+    //             <p className="card-text">{data.sentiment}</p>
+    //         </div>
+    //     </div>
+    // ); 
+
+    return (
+        <div>
+            <table className="table table-striped">
+                <thead className="thead-dark">
+                    <tr>
+                        <th>Date</th>
+                        <th>Log</th>
+                        <th>Sentiment</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderRow}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Journal; 
